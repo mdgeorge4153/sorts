0413d875894f0df5d0ab5e600aa9896fca7bdc77
Michael George <mdgeorge@cs.cornell.edu>
2014-05-02 14:26:31 -0400

    using monad utils, also whitespace fixes

--- bogosort.ml
+++ bogosort.ml
@@ -1,22 +1,22 @@
+open Sorts
 
-module BogoSort (M : SortMonad) = struct
+module Make (M : SortMonad) = struct
 
-  let is_sorted = 
+  module MU = Monad.Utils(M)
+  open M
+  open MU
+
+  let is_sorted () = 
     length >>= fun n ->
-    let rec loop i =
-      if i = n then return true
-      else if lt i (i-1) then return false
-      else loop (i + 1)
-    in loop 1
+    foreach ~from:1 ~until:n ~init:true (fun i acc ->
+      compare i (i-1) >>| (&&) acc
+    )
 
-  let rec bogosort =
-    is_sorted >>= fun b ->
-    if b
-    then return ()
-    else
-      length >>= fun n ->
-      swap (Random.int n) (Random.int n) >>
-      bogosort
+  let sort =
+    length >>= fun n ->
+    dountil ~cond:is_sorted ~init:() (fun () ->
+      swap (Random.int n) (Random.int n)
+    )
 
 end
 
--- heapsort.ml
+++ heapsort.ml
@@ -1,60 +1,56 @@
+open Sorts
 
-module HeapSort (M : SortMonad) = struct
-  module MU = MonadUtils(M)
-  open M
-  open MU
+module Make (M : SortMonad) = struct
+module MU = Monad.Utils(M)
+open M
+open MU
 
-  let sift_down start last =
+let sift_down start last =
 
-    let ind_max i j =
-      if j > last then return i else
-      compare i j >>= fun i_lt_j ->
-      if i_lt_j then return j else return i
-    in
-  
-    let root = start in
-  
-    let rec loop root =
-      if root * 2 + 1 > last then return ()
+  let ind_max i j =
+    if j > last then return i else
+    compare i j >>= fun i_lt_j ->
+    if i_lt_j then return j else return i
+  in
+
+  let root = start in
+
+  let rec loop root =
+    if root * 2 + 1 > last then return ()
+    else
+      let child   = root * 2 + 1 in
+      let biggest = root in
+
+      ind_max biggest child       >>= fun biggest ->
+      ind_max biggest (child + 1) >>= fun biggest ->
+
+      if biggest <> root then
+        swap root biggest >>
+        loop biggest
       else
-	let child   = root * 2 + 1 in
-	let biggest = root in
-  
-	ind_max biggest child       >>= fun biggest ->
-	ind_max biggest (child + 1) >>= fun biggest ->
-  
-	if biggest <> root then
-	  swap root biggest >>
-	  loop biggest
-	else
-	  return ()
-    in loop root
-  
-  let heapify =
-    length >>= fun count ->
-    let start = (count - 2) / 2 in
-  
-    let rec loop start =
-      if start < 0 then return ()
-      else
-	sift_down start (count - 1) >>
-	loop (start - 1)
-    in loop start
-  
-  let sort =
-    heapify >>
-  
-    let rec loop last =
-      Printf.printf "loop %i\n%!" last;
-      if last = 0 then return ()
-      else
-	swap 0 last >>
-	let last = last - 1 in
-	sift_down 0 last >>
-	loop last
-    in
-      length >>= fun count ->
-      loop (count - 1)
+        return ()
+  in loop root
+
+let heapify =
+  length >>= fun count ->
+  let start = (count - 2) / 2 in
+
+  let rec loop start =
+    if start < 0 then return ()
+    else
+      sift_down start (count - 1) >>
+      loop (start - 1)
+  in loop start
+
+let sort =
+  heapify >>= fun ()    ->
+  length  >>= fun count ->
+
+  foreach ~from:(count - 1) ~until:0 ~step:(-1) ~init:() (fun i () ->
+    Printf.printf "loop %i\n%!" i;
+    swap 0 i >>
+    sift_down 0 i
+  )
 
 end
 
