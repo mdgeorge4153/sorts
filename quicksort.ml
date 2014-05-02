93595ec8a2453ab82da795b8a64fcf73e24313be
Michael George <mdgeorge@cs.cornell.edu>
2014-05-02 14:25:28 -0400

    whitespace

--- quicksort.ml
+++ quicksort.ml
@@ -1,6 +1,7 @@
+open Sorts
 
 module Make (M : SortMonad) = struct
-  module MU = MonadUtils(M)
+  module MU = Monad.Utils(M)
   open M
   open MU
 
@@ -14,15 +15,15 @@ module Make (M : SortMonad) = struct
   let partition left right pivot_index =
     (* from wikipedia:
        function partition(array, left, right, pivotIndex)
-	  pivotValue := array[pivotIndex]
-	  swap array[pivotIndex] and array[right]  // Move pivot to end
-	  storeIndex := left
-	  for i from left to right - 1  // left ≤ i < right
-	      if array[i] <= pivotValue
-		  swap array[i] and array[storeIndex]
-		  storeIndex := storeIndex + 1  // only increment storeIndex if swapped
-	  swap array[storeIndex] and array[right]  // Move pivot to its final place
-	  return storeIndex
+          pivotValue := array[pivotIndex]
+          swap array[pivotIndex] and array[right]  // Move pivot to end
+          storeIndex := left
+          for i from left to right - 1  // left ≤ i < right
+              if array[i] <= pivotValue
+                  swap array[i] and array[storeIndex]
+                  storeIndex := storeIndex + 1  // only increment storeIndex if swapped
+          swap array[storeIndex] and array[right]  // Move pivot to its final place
+          return storeIndex
     *)
 
     swap pivot_index right >>
@@ -30,9 +31,9 @@ module Make (M : SortMonad) = struct
     foreach ~from:left ~until:right ~init:left (fun i store_index ->
       compare right i >>= fun pivot_lt_i ->
       if pivot_lt_i
-	then return store_index
-	else swap i store_index >>
-	     return (store_index + 1)
+        then return store_index
+        else swap i store_index >>
+             return (store_index + 1)
     )
     >>= fun store_index ->
 
@@ -44,14 +45,14 @@ module Make (M : SortMonad) = struct
        function quicksort(array, left, right)
        // If the list has 2 or more items
        if left < right
-	   // See "#Choice of pivot" section below for possible choices
-	   choose any pivotIndex such that left ≤ pivotIndex ≤ right
-	   // Get lists of bigger and smaller items and final position of pivot
-	   pivotNewIndex := partition(array, left, right, pivotIndex)
-	   // Recursively sort elements smaller than the pivot (assume pivotNewIndex - 1 does not underflow)
-	   quicksort(array, left, pivotNewIndex - 1)
-	   // Recursively sort elements at least as big as the pivot (assume pivotNewIndex + 1 does not overflow)
-	   quicksort(array, pivotNewIndex + 1, right) *)
+           // See "#Choice of pivot" section below for possible choices
+           choose any pivotIndex such that left ≤ pivotIndex ≤ right
+           // Get lists of bigger and smaller items and final position of pivot
+           pivotNewIndex := partition(array, left, right, pivotIndex)
+           // Recursively sort elements smaller than the pivot (assume pivotNewIndex - 1 does not underflow)
+           quicksort(array, left, pivotNewIndex - 1)
+           // Recursively sort elements at least as big as the pivot (assume pivotNewIndex + 1 does not overflow)
+           quicksort(array, pivotNewIndex + 1, right) *)
     if left >= right then return ()
     else
       let pivot_index = left in
@@ -64,5 +65,5 @@ module Make (M : SortMonad) = struct
     length >>= fun n ->
     quicksort 0 (n - 1)
 
-end 
+end
 
