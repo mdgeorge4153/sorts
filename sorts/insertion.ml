5285c7d6ca1fc6856866f0e3b4445a9aa365e08b
Michael George <mdgeorge@cs.cornell.edu>
2014-05-04 11:55:16 -0400

    optimized insertion sort

--- sorts/insertion.ml
+++ sorts/insertion.ml
@@ -11,13 +11,18 @@ open MU
 
 let sort =
 
+  let finished j =
+    if j <= 0 then return true else
+    compare j (j-1) >>| (<>) Util.Lt
+  in
+
   length >>= fun n ->
   foreach ~from:1 ~until:n ~init:() (fun i () ->
-    foreach ~from:i ~until:0 ~step:(-1) ~init:() (fun j () ->
-      compare (j-1) j >>= function
-        | Gt      -> swap j (j-1)
-        | Lt | Eq -> return ()
-    )
+    printf "inserting element %i" i >>= fun () ->
+    dountil ~cond:finished ~init:i (fun j ->
+      swap (j-1) j >>= fun () ->
+      return (j-1)
+    ) >>| ignore
   )
 
 end
